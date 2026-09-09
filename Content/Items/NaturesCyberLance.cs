da2c8ca8953cfdc547adb55eb6ec09065bdf955f
SenryCC <nigelkeongbarrett@gmail.com>
2026-09-08 22:14:03 -0500

    Update NaturesCyberLance.cs

--- Content/Items/NaturesCyberLance.cs
+++ Content/Items/NaturesCyberLance.cs
@@ -7,12 +7,12 @@ namespace MilkyPasture.Content.Items
 	// This is a basic item template.
 	// Please see tModLoader's ExampleMod for every other example:
 	// https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
-	public class themilker : ModItem
+	public class NaturesCyberLance : ModItem
 	{
 		// The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.MilkyPasture.hjson' file.
 		public override void SetDefaults()
 		{
-			Item.damage = 50;
+			Item.damage = 510;
 			Item.DamageType = DamageClass.Melee;
 			Item.width = 40;
 			Item.height = 40;
@@ -25,7 +25,7 @@ namespace MilkyPasture.Content.Items
 			Item.UseSound = SoundID.Item1;
 			Item.autoReuse = true;
 
-			Item.shoot = ProjectileID.Bomb;
+			Item.shoot = ProjectileID.WoodenArrow;
 			Item.shootSpeed = 100f; // The speed of the projectile (measured in pixels per frame.)
 			//Item.shoot = ProjectileID.WoodenArrowFriendly;
 		}

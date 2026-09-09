04ab8d08cae4c7ab166d62f803d36046d30191bf
SenryCC <nigelkeongbarrett@gmail.com>
2026-09-08 21:47:12 -0500

    Update themilker.cs

--- Content/Items/themilker.cs
+++ Content/Items/themilker.cs
@@ -26,6 +26,7 @@ namespace MilkyPasture.Content.Items
 			Item.autoReuse = true;
 
 			Item.shoot = ProjectileID.Bomb;
+			Item.shootSpeed = 10f; // The speed of the projectile (measured in pixels per frame.)
 			//Item.shoot = ProjectileID.WoodenArrowFriendly;
 		}
 
